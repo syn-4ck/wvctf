ae5ab205cc03500796ad582e617781e7f0202bfb
Julian Fonticoba Mouriz <julian.fonticoba@gmail.com>
2020-04-18 22:26:29 +0200

    Subida del proyecto base

---
+++ README.md
@@ -0,0 +1,13 @@
+# WebVulnCTF
+
+*WVCTF* is a web platform whose goal is provide a vulnerable web aplication that contains some flags to capture.  
+
+## Build and deploy
+
+To build:
+
+mvn clean install
+
+To deploy:
+
+mvn spring-boot:run
---
+++ src/main/java/com/julianfm/wvctf/WvctfApplication.java
@@ -0,0 +1,13 @@
+package com.julianfm.wvctf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WvctfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WvctfApplication.class, args);
+	}
+
+}
